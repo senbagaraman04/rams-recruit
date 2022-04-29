1ead82f0762f88dc23971073f919ec21bcbb6eb6
Senbagaraman Manoharan <senbagaraman04@gmail.com>
2022-04-29 20:46:38 +0530

    feat: added candidate details form (#7)
    Co-authored-by: Senbagaraman <senbagaraman.manoharan@siemens.com>
    closes #6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddCandidateDataComponent } from './components/add-candidate-data/add-candidate-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'adddata', component: AddCandidateDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
