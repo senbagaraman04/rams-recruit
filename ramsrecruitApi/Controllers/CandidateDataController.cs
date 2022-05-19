bc8ed1cd0eba8fa941898471ba991047d38898e9
Senbagaraman <senbagaraman.manoharan@siemens.com>
2022-05-19 20:13:40 +0530

    feat: static candidate data on request #19

---
+++ ramsrecruitApi/Controllers/CandidateDataController.cs
@@ -0,0 +1,66 @@
+﻿using ramsrecruitApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ramsrecruitApi.Controllers
+{
+    public class CandidateDataController : ApiController
+    {
+        // GET: api/CandidateData
+        public IEnumerable<CandidateData> Get()
+        {
+            var cd = new CandidateData();
+            cd.currentctc = "1000";
+            cd.email = "testEmail@suthor.com";
+            cd.experience = "4";
+            cd.name = "Senbagaraman";
+            cd.phonenumber = "23455667";
+            cd.pskills = "Azure, Gmail, gaming";
+            cd.rcompany = "Rams Fleet Management SYstem";
+
+
+            var cd2 = new CandidateData();
+            cd2.currentctc = "31000";
+            cd2.email = "testEmail@author.com";
+            cd2.experience = "14";
+            cd2.name = "raman";
+            cd2.phonenumber = "23455667";
+            cd2.pskills = "Project Manager";
+            cd2.rcompany = "ncovid website";
+
+            List<CandidateData> candid = new List<CandidateData>();
+
+            candid.Add(cd);
+            candid.Add(cd2);
+
+            return candid;
+        }
+
+        // GET: api/CandidateData/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/addCandidateData
+        public void Post([FromBody]string value)
+        {
+             
+            Console.WriteLine("Raman");
+        }
+
+        // PUT: api/CandidateData/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/CandidateData/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ramsrecruitApi/Models/CandidateData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ramsrecruitApi.Models
+{
+    public class CandidateData
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+       public string phonenumber { get; set; }
+
+        public string pskills { get; set; }
+
+        public string experience { get; set; }
+
+        public string rcompany { get; set; }
+public string currentctc { get; set; }
+    }
+}
