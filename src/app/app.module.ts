8071d17ef4411dec44ce4b1519a76d0bf64de17c
Senbagaraman <senbagaraman.manoharan@siemens.com>
2022-07-23 18:34:54 +0530

    feature: empty cards component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponentComponent } from './cards-component/cards-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cards-component/cards-component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardsComponentComponent } from './cards-component.component';
+
+describe('CardsComponentComponent', () => {
+  let component: CardsComponentComponent;
+  let fixture: ComponentFixture<CardsComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardsComponentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CardsComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
