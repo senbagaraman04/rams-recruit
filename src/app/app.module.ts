9fa6dae0442aa04bee6b8e793e4047df5e93758a
Senbagaraman <senbagaraman.manoharan@siemens.com>
2022-07-23 21:28:51 +0530

    feat: add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,10 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -27,14 +31,15 @@ import { LayoutModule } from '@angular/cdk/layout';
     CardsComponentComponent,
     AddcandidateformComponent,
     CandidatelistComponent,
-    RamsrecruitdasboardComponent
+    RamsrecruitdasboardComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatDividerModule, FormsModule, ReactiveFormsModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatMenuModule, MatIconModule, MatButtonModule, LayoutModule
+    MatDividerModule, FormsModule, ReactiveFormsModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatMenuModule, MatIconModule, MatButtonModule, LayoutModule, MatToolbarModule, MatSidenavModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
