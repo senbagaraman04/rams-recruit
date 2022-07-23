9c8ec74dd706869a1f4f614bd4dbc7b47b81094e
Senbagaraman <senbagaraman.manoharan@siemens.com>
2022-07-23 22:01:11 +0530

    chore: updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddcandidateformComponent } from './addcandidateform/addcandidateform.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent,
+  children: [
+     { path: 'dashboard', component: AddcandidateformComponent}
+  ]
+},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
