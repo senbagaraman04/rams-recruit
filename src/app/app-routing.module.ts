4a0612007f7b4684cf79c4d2e102afece1ff5c00
Senbagaraman <senbagaraman.manoharan@siemens.com>
2022-07-24 09:05:20 +0530

    fix: application update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddcandidateformComponent } from './addcandidateform/addcandidateform.component';
+import { CandidatelistComponent } from './candidatelist/candidatelist.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent,
   children: [
-     { path: 'dashboard', component: AddcandidateformComponent}
+     { path: 'dashboard', component: AddcandidateformComponent},
+     {path: 'candidateList', component: CandidatelistComponent},
+     
   ]
 },
   
