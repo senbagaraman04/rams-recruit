d61d78373ee77ca7adbf42998416bfbab8929023
senbagaraman04 <senbagaraman04@gmail.com>
2022-12-26 10:08:22 +0530

    chore: repo cleanup

---
+++ ramsrecruit-backend/src/main/java/com/example/ramsrecruit/RamsrecruitApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramsrecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamsrecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamsrecruitApplication.class, args);
+	}
+
+}
