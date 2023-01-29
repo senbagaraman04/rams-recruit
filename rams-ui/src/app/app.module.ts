fe39d6e5492eb113ce0c29e5eb44eb94e2cc042e
senbagaraman04 <senbagaraman04@gmail.com>
2023-01-29 16:36:19 +0530

    chore: removed unwanted files

--- rams-ui/src/app/app.module.ts
+++ rams-ui/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardsComponentComponent } from './cards-component/cards-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
@@ -16,7 +15,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { RamsrecruitdasboardComponent } from './ramsrecruitdasboard/ramsrecruitdasboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -35,8 +33,6 @@ import { InterviewerListComponent } from './interviewer-list/interviewer-list.co
  @NgModule({
   declarations: [
     AppComponent,
-    CardsComponentComponent,
-    RamsrecruitdasboardComponent,
     HomepageComponent,
     OfferDetailsComponent,
     DasboardwrapperComponent,
