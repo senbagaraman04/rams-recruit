ab455818fdae2b1f3473ce8cd3ac5af140860c01
senbagaraman04 <senbagaraman04@gmail.com>
2023-02-18 14:46:43 +0530

    feat: empty controller

---
+++ ramsrecruit-backend/src/main/java/com/rams/ramsrecruit/controller/LoginController.java
@@ -0,0 +1,29 @@
+/**
+ * https://github.com/senbagaraman04/rams-recruit
+ */
+
+package com.rams.ramsrecruit.controller;
+
+import com.rams.ramsrecruit.entity.Candidate;
+import com.rams.ramsrecruit.entity.Interviewer;
+import com.rams.ramsrecruit.service.InterviewerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+@RequestMapping("/ramsapi")
+@CrossOrigin(origins = "http://localhost:4200")
+public class LoginController {
+
+
+}
